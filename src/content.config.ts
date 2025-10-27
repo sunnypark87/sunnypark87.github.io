132d7a675c7b1ba6920677cffc5764d65da0b375
Jaeseon Park <73125851+sunnypark87@users.noreply.github.com>
2025-10-28 00:22:53 +0900

    library 메인 페이지 & movie 페이지 구조 정리

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
+const post = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/post', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
@@ -12,13 +12,13 @@ const blog = defineCollection({
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
+			thumbnail: image().optional(),
 		}),
 });
 
-const hobby = defineCollection({
+const movie = defineCollection({
 	// Load Markdown and MDX files in the `src/content/hobby/` directory.
-	loader: glob({ base: './src/content/hobby', pattern: '**/*.{md,mdx}' }),
+	loader: glob({ base: './src/content/hobby/movie', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
@@ -26,10 +26,10 @@ const hobby = defineCollection({
 			description: z.string(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-			category: z.string().optional(), // 취미 카테고리 (예: 음악, 영화, 독서 등)
+			watchDate: z.coerce.date(),
+			poster: image(),
+			category: z.string().optional(), 
 		}),
 });
 
-export const collections = { blog, hobby };
+export const collections = { post, movie };
---
+++ src/content/hobby/movie/movie1-parasite.md
@@ -0,0 +1,10 @@
+---
+title: "헤어질 결심"
+description: "헤어질 결심"
+pubDate: 2024-01-10
+watchDate: 2025-10-25
+poster: "../../../assets/movie/movie1.jpg"
+category: "영화"
+---
+
+# 예시 - 헤어질 결심 
