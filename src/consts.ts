81879207abe90aca3e9ff54f9ddce6962a8ea7d0
Jaeseon Park <73125851+sunnypark87@users.noreply.github.com>
2025-11-23 21:21:21 +0900

    블로그 포스트 썸네일 일시 제거

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const TECH_SITE_TITLE = 'LittleBread\'s Blog';
+export const TECH_SITE_TITLE = 'Sunnypark\'s Blog';
 export const HOBBY_SITE_TITLE = 'LittleBread Library';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
